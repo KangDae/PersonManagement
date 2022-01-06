60d695380982f29db654f35a9843efb243013605
eownsl119 <67668143+eownsl119@users.noreply.github.com>
2022-01-06 23:47:03 +0900

    KangDae
    1차 업로드

---
+++ src/person/view/SaramListView.java
@@ -0,0 +1,17 @@
+package person.view;
+
+
+import person.model.SaramVo;
+
+public class SaramListView extends SaramView {
+	
+	@Override
+	public SaramVo display() {
+		System.out.println(":::::사람 정보:::::");
+		
+		
+		return null;
+	    
+	}
+
+}
---
+++ src/person/view/menuView.java
@@ -0,0 +1,37 @@
+package person.view;
+
+import person.rc;
+import person.model.SaramVo;
+
+public class menuView extends SaramView {
+
+	 
+	@Override
+	public SaramVo display() {
+		System.out.println(":::::메뉴:::::");
+		
+		int no;
+		while (true) {
+			try {
+				System.out.println("1.입력(Create) 2.출력(Read) 3.수정(Update) 4.삭제(delete) 5.종료");
+				System.out.print("����:");
+				 no = scan.nextInt();
+
+				while (no > 5 || no < 1) {
+					System.out.println("범위를 넘었습니다.");
+					System.out.println("입력:");
+					no = scan.nextInt();
+				}
+				break;
+			} catch (Exception e) {
+				System.out.println("숫자만 입력하세요");
+				scan.next();
+				continue;
+			}
+		}
+
+		rc.no = no;
+		return null;
+	}
+
+}
